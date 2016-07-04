64c44f1b5be4aed903ec25c96b1053684d31670a
Greg Pritchard <gregcraig23@gmail.com>
2016-07-04 04:35:39 -0500

    add more things

---
+++ GergsLibraryOfThings/gerg/things/network/ip/IP.cs
@@ -0,0 +1,18 @@
+﻿namespace GergsLibraryOfThings.gerg.things.network.ip
+{
+    public class IP
+    {
+        private IPType ipType;
+        private uint ipv4ip;
+
+
+        public IP(uint ipv4) {
+            this.ipv4ip = ipv4;
+            this.ipType = IPType.IPv4;
+        }
+
+        private void Validate() {
+
+        }
+    }
+}
---
+++ GergsLibraryOfThings/gerg/things/network/ip/IPType.cs
@@ -0,0 +1,8 @@
+﻿namespace GergsLibraryOfThings.gerg.things.network.ip
+{
+    public enum IPType
+    {
+        IPv4,
+        IPv6
+    }
+}
