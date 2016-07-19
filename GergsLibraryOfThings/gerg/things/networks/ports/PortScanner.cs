56c289a2a5f71398faf206803768542312465dbd
Greg Pritchard <gregcraig23@gmail.com>
2016-07-19 01:42:06 -0500

    Added a PortScanner

---
+++ GergsLibraryOfThings/gerg/things/networks/ports/PortScanner.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+
+namespace GergsLibraryOfThings.gerg.things.networks.ports
+{
+    public class PortScanner
+    {
+        private List<int> openPorts = new List<int>();
+        private IPAddress ipAddress;
+        private int[] portRange = new int[2];
+
+        public PortScanner(IPAddress address, int port) {
+            ipAddress = address;
+            portRange[0] = portRange[1] = port;
+        }
+
+        public PortScanner(string address, int port) {
+            if(!IPAddress.TryParse(address, out ipAddress)) {
+                //Error
+            }
+            else {
+                portRange[0] = portRange[1] = port;
+            }
+        }
+
+        public PortScanner(string address, int minPort, int maxPort) {
+            if(!IPAddress.TryParse(address, out ipAddress)) {
+                //Error
+            }
+            else {
+                portRange[0] = minPort;
+                portRange[1] = maxPort;
+            }
+        }
+
+        public void ScanForOpenPorts(bool printResults = false) {
+            TcpClient tcpClient = new TcpClient();
+            for(int i = portRange[0]; i <= portRange[1]; i++) {
+                try {
+                    tcpClient.Connect(ipAddress, i);
+                    openPorts.Add(i);
+
+                    if (printResults) {
+                        Console.WriteLine(i + " - OPEN");
+                    }
+
+                } catch (Exception) {
+
+                    if (printResults) {
+                        Console.WriteLine(i + " - CLOSED");
+                    }
+
+                }
+            }
+        }
+
+        public List<int> GetOpenPorts() {
+            return openPorts;
+        }
+
+        public void PrintOpenPorts() {
+            foreach(int openPort in this.openPorts) {
+                Console.WriteLine(openPort + " - OPEN");
+            }
+        }
+    }
+}
---
+++ GergsLibraryOfThingsTests/gerg/things/networks/ports/PortScannerTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net.Sockets;
+using System.Net;
+
+//Tests dont pass unless you grant this program firewall access (I think, not too familiar with network programming)
+//TODO: Add tests for a range of ports
+namespace GergsLibraryOfThings.gerg.things.networks.ports.Tests
+{
+    [TestClass()]
+    public class PortScannerTests
+    {
+        private int minPort = 45594;
+        private TcpListener listener;
+        private static string ipStr = "127.0.0.1";
+
+        [TestMethod()]
+        public void ScanForOpenPortsTest()
+        {
+            //Arrange
+            IPAddress IP = IPAddress.Parse(ipStr);
+
+            listener = new TcpListener(IP, minPort);
+            listener.Start();
+
+            PortScanner ps = new PortScanner(IP, minPort);
+
+            //Act
+            ps.ScanForOpenPorts();
+            listener.Stop();
+
+            //Assert
+            Assert.AreEqual(ps.GetOpenPorts()[0], minPort);
+        }
+    }
+}
